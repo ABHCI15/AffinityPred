fa62f5ac3862499aef7eedb089934505827c44e6
ABHCI15 <113409577+ABHCI15@users.noreply.github.com>
2025-09-17 17:08:41 -0700

    Updated model hyperparams + added inference file

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 15
 #SBATCH --gres=gpu:1
-#SBATCH --mem 77gb
+#SBATCH --mem 120G
 #SBATCH --output=trainca_%A.log
 
 conda activate gen_ca
-python /home/nroethler/Code/abhiram/chemmap/AffinityPred/train.py
+python /home/nroethler/Code/abhiram/chemmap/AffinityPred/train_caAtt.py
